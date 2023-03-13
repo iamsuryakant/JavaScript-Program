70b9825458fa6e5ffd3e499441f12d8293ef9773
iamsuryakant <suryakantthakur825@gmail.com>
2023-03-13 11:06:14 +0530

    Added codes

---
+++ Test/script.js
@@ -0,0 +1,41 @@
+
+// // Variables
+// console.log(ne);
+// // console.log(job);
+// // console.log(age);
+
+
+
+// var ne = "Surya";
+// let job = "Software Engineer";
+// const age = 25;
+
+
+// // Functions
+
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// // console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//     return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//     return a + b;
+// }
+
+// const addArrow = (a, b) => a + b;
+
+
+const jessica = {
+    firstName: "Jessica",
+    lastName: "Williams",
+    age: 27,
+}
+
+const jessicaCopy = jessica;
+
+console.log(jessicaCopy);
+jessica.lastName = "Davis";
+console.log(jessica);
